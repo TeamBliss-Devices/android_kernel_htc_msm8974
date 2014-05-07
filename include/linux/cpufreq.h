79242d535cf0cdaee2252ed63334bd97b3377516
Sean Hoyt <seanhoyt963@gmail.com>
2014-05-07 09:45:17 -0500

    Add some cpu gov's
    hyper, intellidemand, interactivex
    
    Conflicts:
    	arch/arm/configs/cm_m8_defconfig
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	drivers/cpufreq/cpufreq_adaptive.c

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -321,6 +321,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
 extern struct cpufreq_governor cpufreq_gov_elementalx;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_elementalx;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellidemand)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
