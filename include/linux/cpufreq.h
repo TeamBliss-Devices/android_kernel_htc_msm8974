940162ddf0e565423dc9f5a0f2c5c357d931fb48
Shaun Eaton <shaunsibu@gmail.com>
2014-11-16 09:38:20 -0700

    Fix building from Commit derps, and change cpu idle time.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -82,7 +82,8 @@ struct cpufreq_policy {
 
 	unsigned int		min;    
 	unsigned int		max;    
-	unsigned int		cur;    
+	unsigned int		cur;
+	unsigned int        util;    
 	unsigned int		policy; 
 	struct cpufreq_governor	*governor; 
 
